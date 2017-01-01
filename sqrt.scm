796a4ee80cec25a9d3a118189f147fcbd9c11d1d
Alex Lu <iamalexlu@gmail.com>
2017-01-01 21:09:58 +0800

    more beautiful indent

--- sqrt.scm
+++ sqrt.scm
@@ -1,25 +1,31 @@
+
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
-    guess
-    (sqrt-iter (improve guess x) 
-                x)))
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
 
 (define (improve guess x)
-  (average (/ x guess) guess))
+  (average (/ x guess)
+           guess))
 
 (define (average x y)
-  (/ (+ x y) 
-  2))
+  (/ (+ x y)
+     2))
 
 (define (good-enough? guess x)
-  (< (abs (-  (* guess guess) x)) 0.001))
+  (< (abs (- (square guess) x))
+     0.001))
+
+(define (square x)
+  (* x x))
 
 (define (abs x)
   (if (> x 0)
-    x
-    (- x)))
+      x
+      (- x)))
 
 (define (sqrt x)
   (sqrt-iter 1.0 x))
 
-(sqrt 3)
+(sqrt 4)
