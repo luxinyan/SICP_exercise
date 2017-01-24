7e34d6027b02d6c6084d14e9f26c28733d51407a
Alex Lu <iamalexlu@gmail.com>
2017-01-24 16:58:43 +0800

    finish 2-18

---
+++ Chapter2/2-18.scm
@@ -0,0 +1,13 @@
+(define (append list1 list2)   
+  (if (null? list1)
+      list2
+      (cons (car list1) 
+            (append (cdr list1) list2))))
+
+(define (reverse items)
+  (if (null? items)
+      items
+      (append (reverse (cdr items))
+              (list (car items)))))
+
+(reverse (list 1 2 3))
