4737ecbc79a0bfec86b411dcee49caa85b6e34cd
Alex Lu <iamalexlu@gmail.com>
2017-01-24 17:35:11 +0800

    delete unnecessary function

--- Chapter2/2-27.scm
+++ Chapter2/2-27.scm
@@ -1,17 +1,5 @@
 (define x (list (list 1 2) (list 3 4)))
 
-(define (append list1 list2)   
-  (if (null? list1)
-      list2
-      (cons (car list1) 
-            (append (cdr list1) list2))))
-
-(define (reverse items)
-  (if (null? items)
-      items
-      (append (reverse (cdr items))
-              (list (car items)))))
-
 (define (deep-reverse items)
   (if (null? items)
       items
@@ -19,6 +7,5 @@
           (list (if (pair? (car items)) 
                     (deep-reverse (car items)) 
                     (car items))))))
-
 
 (deep-reverse x)
--- Chapter2/2-28.scm
+++ Chapter2/2-28.scm
@@ -1,11 +1,5 @@
 (define x (list (list 1 3) (list 3 4) 5))
 
-(define (append list1 list2)   
-  (if (null? list1)
-      list2
-      (cons (car list1) 
-            (append (cdr list1) list2))))
-
 (define (fringe items)
   (cond ((null? items) items)
         ((not (pair? items)) (list items))
