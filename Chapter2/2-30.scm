a2b91a1d1d98ff629f3461d597d2bde0a1649046
Alex Lu <iamalexlu@gmail.com>
2017-01-25 15:02:33 +0800

    finish 2-30

---
+++ Chapter2/2-30.scm
@@ -0,0 +1,18 @@
+(define (square-tree tree)
+  (cond ((null? tree) '())
+        ((not (pair? tree)) (* tree tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (* sub-tree sub-tree)))
+       tree))
+
+
+
+(define a (list 1 2 (list 2 4) 5 6))
+
+(square-tree a)
