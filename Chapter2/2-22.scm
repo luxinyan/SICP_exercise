04ea9626eea703cadf6ed2e92c1aa7e9cb934d70
Alex Lu <iamalexlu@gmail.com>
2017-06-04 13:45:06 +0800

    add 2-22

---
+++ Chapter2/2-22.scm
@@ -0,0 +1,15 @@
+#lang sicp
+
+(define (square x) (* x x))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
+
+(define my-list (list 1 2 3 4))
+(square-list my-list)
