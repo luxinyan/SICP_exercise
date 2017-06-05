f4224724451d286841f4f9c6b95443ea29607b2f
Alex Lu <iamalexlu@gmail.com>
2017-06-05 20:40:48 +0800

    Finish 2-23

--- Chapter2/2-23.scm
+++ Chapter2/2-23.scm
@@ -1,10 +1,14 @@
-(define (for-each proc items)
-  (if (null? items)
-      '()
-      ((proc (car items))
-       (for-each proc (cdr items)))))
+#lang sicp
 
- (for-each (lambda (x) 
-             (newline) (display x)) 
-           (list 1 2 3 4)) 
+(define (for-each proc items) 
+  (cond ((not (null? items)) 
+         (proc (car items)) 
+         (for-each proc (cdr items))))) 
+
+
+(for-each (lambda (x) 
+            (newline) (display x)) 
+          (list 1 2 3 4 5))
+
+
  
